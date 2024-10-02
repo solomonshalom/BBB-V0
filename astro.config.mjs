b9573497b1bb356ff01b1a38d02d2f3f51961128
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-10-02 11:00:36 +0530

    Jesus W!
    Changed the output to reflect SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [partytown()],
-  output: 'server',
+  output: 'static',
   adapter: vercel()
 });
