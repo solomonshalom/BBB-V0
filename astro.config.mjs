f704f0043258f1f5abbcd95cd559b044a7636165
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-10-04 01:06:15 +0530

    Jesus W!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 
-import vercel from '@astrojs/vercel/static';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()],
-  output: 'server',
-  adapter: vercel()
+  integrations: [partytown()]
 });
